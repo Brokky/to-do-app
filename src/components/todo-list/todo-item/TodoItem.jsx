6729bb066586ac08dedf586f1672ba448dd9843c
Daniel Brokk <danielbrokky@gmail.com>
2022-12-28 16:47:52 +0200

    add changing style when checkbox is clicked

--- src/components/todo-list/todo-item/TodoItem.jsx
+++ src/components/todo-list/todo-item/TodoItem.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TodoItem(props) {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const spanStyle = {
+        textDecoration: isChecked ? 'line-through' : 'none',
+        fontStyle: isChecked ? 'italic' : 'inherit',
+        color: isChecked ? '#888' : 'inherit',
+    };
+
     return (
         <li>
-            <input type="checkbox" />
-            <span>{props.content}</span>
+            <input type="checkbox" checked={isChecked} onChange={(event) => setIsChecked(event.target.checked)} />
+            <span style={spanStyle}>{props.content}</span>
             <button onClick={props.removeTask}>Delete</button>
         </li>
     )
